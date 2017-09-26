0e249fdea7b1606847724710470b845d92a2d168
sikanderc <sikanderchowhan@gmail.com>
2017-09-26 09:12:21 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :title_includes_keywords
+
+
+
+  def title_includes_keywords
+    phrases = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    unless self.title != nil && phrases.any? do |phrase|
+      self.title.include?(phrase)
+    end
+      errors.add(:title, "True Facts")
+    end
+  end
+
 end
